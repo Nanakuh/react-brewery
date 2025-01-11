43cb87f5b01fc118d65accbdfb00e3e62896a3d5
Ana Gil <anagilrodrig@gmail.com>
2025-01-11 21:13:53 +0100

    Fix label

--- src/constants/BreweryTypes.ts
+++ src/constants/BreweryTypes.ts
@@ -4,7 +4,7 @@ export const BREWERYTYPES = [
   { label: "CerveRegional brewery", value: "regional" },
   { label: "Brewpub restaurant", value: "brewpub" },
   { label: "Very large brewery", value: "large" },
-  { label: "CervecerBrewery in planning", value: "planning" },
+  { label: "Brewery in planning", value: "planning" },
   { label: "Bar without a brewery", value: "bar" },
   { label: "Contract brewery", value: "contract" },
   { label: "Brewery incubator", value: "proprietor" },
